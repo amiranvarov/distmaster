ac8aaa35171512ce37a4850bf0219dea7d6f572c
amir <lifemount@gmail.com>
2018-10-18 19:50:17 +0500

    add yarn.lock

--- src/db.ts
+++ src/db.ts
@@ -9,4 +9,19 @@ export default class DB {
         DB.mongo = client.db('dist-bot');
         console.log('MongoDB connected')
     }
+
+    static async getNextSequenceValue(sequenceName){
+
+        const sequenceDocument = await DB.mongo.collection('counters').findOneAndUpdate(
+            { _id: sequenceName },
+            { $inc: {sequence_value:1 }},
+            { returnNewDocument: true }
+        );
+        if(!sequenceDocument) {
+            return;
+        }
+        const id = sequenceDocument.value.sequence_value;
+        console.log('ID!!!', id);
+        return id;
+    }
 }
