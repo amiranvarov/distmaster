bf1426af06a06b2c8b7adb870b5a000dc0e9a44e
amir <lifemount@gmail.com>
2018-10-21 03:14:14 +0500

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import Messanger from './lib/Messanger'
 import DB from './db'
-
+import * as express from 'express'
 
 
 
 (async function () {
+
+    express()
+        .get('/test', (req, res) => res.send('hello world'))
+        .listen(process.env.PORT, () => console.log(`Listening on ${ process.env.PORT }`));
+
     // await DB.init();
     const messanger = new Messanger();
     await messanger.listen();
