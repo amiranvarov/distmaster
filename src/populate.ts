3fd8268731926634e8fe11dad7ae9fcbcc88bb7f
amir <lifemount@gmail.com>
2018-10-21 02:53:39 +0500

    use db connection uri

--- src/populate.ts
+++ src/populate.ts
@@ -4,11 +4,14 @@ import * as SeedProducts from './Data'
 (async function () {
 
     await DB.init();
+    // sequence
+    await DB.mongo.collection('counters').insertOne({
+        "_id" : "invoiceid", "sequence_value" : 669
+    });
 
     // await DB.mongo.collection('positions').deleteMany({});
     await DB.mongo.collection('positions').insertMany([
-        ...SeedProducts.CocaCola,
-        ...SeedProducts.Bliss
+        ...SeedProducts.CocaCola
     ])
 
     console.log('Population completed')
