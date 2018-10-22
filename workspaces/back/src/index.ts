69c9f7a7687ff19213dfdd7ce9ab2be1874be580
amir <lifemount@gmail.com>
2018-10-22 15:34:56 +0500

    feat(Back): Serve React bundle

--- workspaces/back/src/index.ts
+++ workspaces/back/src/index.ts
@@ -1,18 +1,21 @@
 import Messanger from './lib/Messanger'
 import DB from './db'
 import * as express from 'express'
+import * as path from 'path'
 
 
+const PORT = process.env.PORT || 3003;
+const REACT_BUNDLE_LOCATION = path.resolve('../front/build');
 
 (async function () {
 
-    const PORT = process.env.PORT || 3000;
     express()
+        .use(express.static(REACT_BUNDLE_LOCATION))
         .get('/test', (req, res) => res.send('hello world'))
         .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-    const messanger = new Messanger();
-    await messanger.listen();
+    const messenger = new Messanger();
+    await messenger.listen();
     await DB.init()
 
 })();
